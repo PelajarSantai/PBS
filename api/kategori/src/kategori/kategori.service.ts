f9e0d3ad8405bed14aaaf719c61aafcfe0470bdb
PelajarSantai <muhammadraditia25@gmail.com>
2026-03-05 15:00:52 +0700

    test

---
+++ api/kategori/src/kategori/kategori.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateKategoriDto } from './dto/create-kategori.dto';
+import { UpdateKategoriDto } from './dto/update-kategori.dto';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class KategoriService {
+  // buat constructor untuk prisma service
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createKategoriDto: CreateKategoriDto) {
+    return 'This action adds a new kategori';
+  }
+  // tampilkan seluruh data kategori
+
+  async findAll() {
+    // return `This action returns all kategori`;
+    // tampilkan data kategori
+    const data = await this.prisma.kategori.findMany();
+    return data;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} kategori`;
+  }
+
+  update(id: number, updateKategoriDto: UpdateKategoriDto) {
+    return `This action updates a #${id} kategori`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} kategori`;
+  }
+}
